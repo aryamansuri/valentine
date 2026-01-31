028e1a8ca577b5c319d4526c79512e9fbc59546f
aryamansuri <arysuri21@gmail.com>
2026-01-31 18:37:04 -0500

    Initial valentine app

---
+++ script.js
@@ -0,0 +1,18 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const result = document.getElementById("result");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 80 - 40;
+
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+noBtn.addEventListener("click", () => {
+  alert("Wrong choice 😌");
+});
+
+yesBtn.addEventListener("click", () => {
+  result.classList.remove("hidden");
+});
