602964bb5968a2961666f4f30d018aca124404ba
aryamansuri <arysuri21@gmail.com>
2026-02-01 09:12:52 -0500

    more

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 const result = document.getElementById("result");
 
-noBtn.addEventListener("mouseover", () => {
-  const x = Math.random() * 500 - 250;
-  const y = Math.random() * 300 - 150;
+const buttonsArea = document.querySelector(".buttons");
 
-  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+document.addEventListener("mousemove", (e) => {
+  const rect = noBtn.getBoundingClientRect();
+
+  const dx = e.clientX - (rect.left + rect.width / 2);
+  const dy = e.clientY - (rect.top + rect.height / 2);
+  const distance = Math.sqrt(dx * dx + dy * dy);
+
+  if (distance < 120) {
+    const angle = Math.atan2(dy, dx);
+    const moveX = Math.cos(angle) * -200;
+    const moveY = Math.sin(angle) * -200;
+
+    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+  }
 });
 
 noBtn.addEventListener("click", () => {
